ca1d776309dd7599780767b3a320b59d1ef1a1e7
Kevin Kim <kevin.kim1030@gmail.com>
2019-09-20 02:33:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  }
+  
